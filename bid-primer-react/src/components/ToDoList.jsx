8587e769eeb5bbd71344e922eab15b26484ffffd
segmauricio <mauribox28@gmail.com>
2023-01-28 09:47:34 -0300

    Implementation of a component on the App.js and removal of several files

---
+++ bid-primer-react/src/components/ToDoList.jsx
@@ -0,0 +1,16 @@
+const ToDoList = () => {
+    return (
+        <>
+        <h2>Things I need to do:</h2>
+        <ul>
+          <li>Learn React</li>
+          <li>Climb Mt. Everest</li>
+          <li>Run a marathon</li>
+          <li>Feed the dogs</li>
+        </ul>
+        </>
+        
+    );
+}
+
+export default ToDoList;
